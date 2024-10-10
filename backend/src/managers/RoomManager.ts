6a6f3a89f6ec23bc8e56868a8885535d39494d40
Viraj Bhagiya <virajbhagiya@gmail.com>
2024-10-10 20:20:01 +0530

    complete

--- backend/src/managers/RoomManager.ts
+++ backend/src/managers/RoomManager.ts
@@ -23,23 +23,31 @@ export class RoomManager {
         user1.socket.emit("send-offer", {
             roomId
         })
+
+        user2.socket.emit("send-offer", {
+            roomId
+        })
     }
 
-    onOffer(roomId: string, sdp: string){
-        const user2 = this.rooms.get(roomId)?.user2;
-        console.log("On offer!");
-        console.log("User2 is " + user2);
-        user2?.socket.emit("offer", {
+    onOffer(roomId: string, sdp: string, senderSocketId: string){
+        const room = this.rooms.get(roomId);
+        if(!room){
+            return;
+        }
+        const receivingUser = room.user1.socket.id === senderSocketId ? room.user2: room.user1;
+        receivingUser?.socket.emit("offer", {
             sdp,
             roomId
         })
     }
 
-    onAnswer(roomId: string, sdp: string){
-        const user1 = this.rooms.get(roomId)?.user1;
-        console.log("On answer!");
-        console.log("User1 is " + user1);
-        user1?.socket.emit("answer", {
+    onAnswer(roomId: string, sdp: string, senderSocketId: string){
+        const room = this.rooms.get(roomId);
+        if(!room){
+            return;
+        }
+        const receivingUser = room.user1.socket.id === senderSocketId ? room.user2: room.user1;
+        receivingUser?.socket.emit("answer", {
             sdp,
             roomId
         })
@@ -51,10 +59,8 @@ export class RoomManager {
             return;
         }
         const receivingUser = room.user1.socket.id === senderSocketId ? room.user2: room.user1;
-        receivingUser.socket.send("add-ice-candidate", ({candidate, type})) 
-
+        receivingUser.socket.emit("add-ice-candidate", ({candidate, type})) 
     }
-
     
     generate(){
         return GLOBAL_ROOM_ID++;
--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -6,7 +6,7 @@ const URL = "http://localhost:3000";
 
 export const Room = ({
     name,
-    localAudioTrack,
+    localAudioTrack, 
     localVideoTrack
 }: {
     name: string;
@@ -14,104 +14,151 @@ export const Room = ({
     localVideoTrack: MediaStreamTrack | null;
 }) => {
     const [searchParams, setSearchParams] = useSearchParams();
-    const [ lobby, setLobby] = useState(true);
+    const [lobby, setLobby] = useState(true);
     const [socket, setSocket] = useState<null | Socket>(null);
     const [sendingPc, setSendingPc] = useState<null | RTCPeerConnection>(null);
     const [receivingPc, setReceivingPc] = useState<null | RTCPeerConnection>(null);
     const [remoteVideoTrack, setRemoteVideoTrack] = useState<MediaStreamTrack | null>(null);
     const [remoteAudioTrack, setRemoteAudioTrack] = useState<MediaStreamTrack | null>(null);
     const [remoteMediaStream, setRemoteMediaStream] = useState<MediaStream | null>(null);
-    const remoteVideoRef = useRef<HTMLVideoElement | undefined>();
+    const remoteVideoRef = useRef<HTMLVideoElement>();
     const localVideoRef = useRef<HTMLVideoElement>();
 
     useEffect(() => {
         const socket = io(URL);
 
         socket.on("send-offer", async ({roomId}) => {
+            console.log("Sending Offer!");
             setLobby(false);
             const pc = new RTCPeerConnection();
+
             setSendingPc(pc);
             if(localVideoTrack){
+                console.error("Added Track!")
+                console.log(localVideoTrack);
                 pc.addTrack(localVideoTrack);
             }
             if(localAudioTrack){
+                console.error("Added Track!")
+                console.log(localAudioTrack);
                 pc.addTrack(localAudioTrack);
             }
 
             pc.onicecandidate = async (e) => {
+                console.log("Receiving Ice Candidate Locally!");
                 if(e.candidate){
                     socket.emit("add-ice-candidate", {
                         candidate: e.candidate,
-                        type: "sender"
+                        type: "sender",
+                        roomId
                     })
                 }
             }
 
             pc.onnegotiationneeded = async () => {
-                setTimeout(async () => {
-                    const sdp = await pc.createOffer();
-                    // @ts-ignore
-                    pc.setLocalDescription(sdp.sdp);
-                    socket.emit('offer', {
-                        roomId,
-                        sdp
-                    })
-                }, 2000);
+                console.log("On Negotiation Needed, Sending Offer!");
+                const sdp = await pc.createOffer();
+                // @ts-ignore
+                pc.setLocalDescription(sdp);
+                socket.emit('offer', {
+                    sdp,
+                    roomId
+                })
             }
         });
 
         socket.on("offer", async ({roomId, sdp: remoteSdp}) => {
+            console.log("Received Offer!");
             setLobby(false);
+
             const pc = new RTCPeerConnection();
             pc.setRemoteDescription(remoteSdp);
+
             const sdp = await pc.createAnswer();
             // @ts-ignore
-            pc.setLocalDescription(sdp.sdp);
+            pc.setLocalDescription(sdp);
             const stream = new MediaStream();
             if(remoteVideoRef.current){
                 remoteVideoRef.current.srcObject = stream;
             }
+
             setRemoteMediaStream(stream);
 
             setReceivingPc(pc);
+            window.pcr = pc;
+            pc.ontrack = (e) => {
+                alert("Ontrack!");
+                // const {track, type} = e;
+                // if(type == 'audio'){
+                //     // setRemoteAudioTrack(track);
+                //     // @ts-ignore
+                //     remoteVideoRef.current.srcObject.addTrack(track);
+                // } else{
+                //     // setRemoteVideoTrack(track);
+                //     // @ts-ignore
+                //     remoteVideoRef.current.srcObject.addTrack(track);
+                // }
+                // // @ts-ignore
+                // remoteVideoRef.current?.play();
+            }
 
             pc.onicecandidate = async (e) => {
+                console.log("On Ice Candidate on receiving side!");
+                if(!e.candidate){
+                    return;
+                }
                 if(e.candidate){
                     socket.emit("add-ice-candidate", {
                         candidate: e.candidate,
-                        type: "receiver"
+                        type: "receiver",
+                        roomId
                     })
                 }
             }
 
-            pc.ontrack = (({track, type}) => {
-                if(type == 'audio'){
-                    // setRemoteAudioTrack(track);
-                    // @ts-ignore
-                    remoteVideoRef.current.srcObject.addTrack(track);
-                } else{
-                    // setRemoteVideoTrack(track);
-                    // @ts-ignore
-                    remoteVideoRef.current.srcObject.addTrack(track);
-                }
-                // @ts-ignore
-                remoteVideoRef.current?.play();
-            })
             socket.emit("answer", {
                 roomId,
                 sdp: sdp
             });
+
+            setTimeout(() => {
+                const track1 = pc.getTransceivers()[0].receiver.track;
+                const track2 = pc.getTransceivers()[1].receiver.track;
+                console.log(track1);
+                if(track1.kind == "video"){
+                    setRemoteAudioTrack(track2);
+                    setRemoteVideoTrack(track1);
+                } else{
+                    setRemoteAudioTrack(track1);
+                    setRemoteVideoTrack(track2);
+                }
+
+                // @ts-ignore
+                remoteVideoRef.current.srcObject.addTrack(track1);
+                // @ts-ignore
+                remoteVideoRef.current.srcObject.addTrack(track2);
+                // @ts-ignore
+                remoteVideoRef.current.play();
+                // if (type == 'audio') {
+                //     // setRemoteAudioTrack(track);
+                //     // @ts-ignore
+                //     remoteVideoRef.current.srcObject.addTrack(track)
+                // } else {
+                //     // setRemoteVideoTrack(track);
+                //     // @ts-ignore
+                //     remoteVideoRef.current.srcObject.addTrack(track)
+                // }
+                // //@ts-ignore
+            }, 5000)
         });
         
         socket.on("answer", ({roomId, sdp: remoteSdp}) => {
             setLobby(false);
             setSendingPc(pc => {
-                pc?.setRemoteDescription({
-                    type: "answer",
-                    sdp: remoteSdp
-                })
+                pc?.setRemoteDescription(remoteSdp)
                 return pc;
-            })
+            });
+            console.log("Loop Closed!");
         })
 
         socket.on("lobby", () => {
@@ -119,16 +166,28 @@ export const Room = ({
         })
 
         socket.on("add-ice-candidate", ({candidate, type}) => {
+            console.log("Add Ice Candidate from Remote!");
+            console.log({candidate, type});
             if(type== "sender"){
                 setReceivingPc(pc => {
+                    if(!pc){
+                        console.error("Receiving PC not found!");
+                    } else{
+                        console.error(pc.ontrack);
+                    }
                     pc?.addIceCandidate(candidate);
                     return pc;
                 })
             } else{
-                setReceivingPc(pc => {
+                setSendingPc(pc => {
+                    if(!pc){
+                        console.error("Seding PC not found!");
+                    } else{
+                        console.error(pc.ontrack);
+                    }
                     pc?.addIceCandidate(candidate);
                     return pc;
-                })
+                });
             }
         })   
 
